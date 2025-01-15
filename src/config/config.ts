863c4fa09c1743d60c8a226cebc31c63a0786ced
Cristian Girlea <cristiangirlea@gmail.com>
2025-01-15 17:52:42 +0200

    remove log

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log('Laravel API:',process.env.LARAVEL_API);
 export const LARAVEL_API = process.env.LARAVEL_API || 'http://localhost:8080/api';
